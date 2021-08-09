9118947b09a8001da3bf09777a9b9581a548f740
ruebenmonsen <85364715+ruebenmonsen@users.noreply.github.com>
2021-08-09 10:59:32 +0200

    style changes and validation

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,18 +1,32 @@
 const form = document.querySelector('#user-form');
 const list = document.querySelector('#shopping-list-content');
+const userError = document.querySelector('#user-error');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
     listItem = form.elements['item-content'].value; // Note to self don't give an element the id item
-    list.innerHTML += `
-    <li class="list-group-item list-group-item-action">${listItem}<button type="button" class="btn-close" aria-label="Close"></button></li>
-    `
-    ResetInputField();
+    if (listItem !== '') {
+        list.innerHTML += `
+        <div><li class="list-group-item">${listItem}<button type="button" class="btn-close" aria-label="Close"></button></li></div>
+        `
+        ResetInputField();
+        ResetErrorField();
+    }
+    else {
+        // error message here
+        userError.textContent = 'Skriv in en vara i fältet ovan';
+        userError.style.color = 'red';
+    }
+
 });
 
 function ResetInputField() {
     form.elements['item-content'].value = '';
 };
+
+function ResetErrorField() {
+    userError.textContent = '';
+}
 
 list.addEventListener('click', e => {
     e.preventDefault();
@@ -20,8 +34,8 @@ list.addEventListener('click', e => {
 });
 
 list.addEventListener('click', e => {
-    if(e.target.classList[0] == 'btn-close'){
+    if (e.target.classList[0] == 'btn-close') {
         e.target.parentElement.remove();
 
     }
- });
+});
